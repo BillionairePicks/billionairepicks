4b507edc2dd6221c1ca04495445c06b21d94c837
BillionairePicks <billionairepicks999@gmail.com>
2025-09-16 01:59:02 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,9 +1,13 @@
 // Initialize Firebase (insert your Firebase config here)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAsSHeTgN10bkdywhlJoi_p4ni7bxUAm6c",
+  authDomain: "stat-stake.firebaseapp.com",
+  projectId: "stat-stake",
+  storageBucket: "stat-stake.firebasestorage.app",
+  messagingSenderId: "128167594111",
+  appId: "1:128167594111:web:20d36adbae750494efd74a",
+  measurementId: "G-H4HSTHZXX1"
 };
 firebase.initializeApp(firebaseConfig);
 
