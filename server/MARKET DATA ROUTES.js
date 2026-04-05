8f7f15d121b08b08f863d2d958bcde26838b9b53
BillionairePicks <billionairepicks999@gmail.com>
2026-04-05 16:42:47 +0200

    Delete server directory

--- server/MARKET DATA ROUTES.js
+++
@@ -1,7 +0,0 @@
-router.get("/crypto/:symbol", async (req,res)=>{
-    const {symbol} = req.params
-    const response = await axios.get(
-      `https://api.binance.com/api/v3/ticker/24hr?symbol=${symbol}`
-    )
-    res.json(response.data)
-})
